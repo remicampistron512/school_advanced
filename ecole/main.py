785136e51e002c6ede00cef420b3b7ed84a24109
Rémi Campistron <remi.campistron@gmail.com>
2025-12-04 13:46:48 +0100

    amélioration de l'affichage dans main

--- ecole/main.py
+++ ecole/main.py
@@ -24,35 +24,50 @@ Bienvenue dans notre école
     # affichage de la liste des cours, leur enseignant et leurs élèves
     school.display_courses_list()
 
-    print(school.get_course_by_id(1))
-    print(school.get_course_by_id(2))
-    print(school.get_course_by_id(9))
-
+    print("----- AFFICHAGE DE COURS PAR LEUR ID -----")
+    print(school.get_course_by_id(7))
+    print ("")
+    print("----- AFFICHAGE D'ÉTUDIANTS PAR LEUR ID ----- ")
     print(school.get_student_by_id(1))
-
+    print("")
+    print("----- AFFICHAGE DE PROFESSEUR PAR LEUR ID ----- ")
     print(school.get_teacher_by_id(4))
-
+    print("")
+    print("----- AFFICHAGE DE L'ADRESSE PAR LEUR ID ------")
     print(school.get_address_by_id(1))
+    print("")
 
+    print("----- AFFICHAGE DE TOUS LES PROFESSEURS-----")
     teachers = school.get_all_teachers()
+    for teacher in teachers:
+        print(teacher)
+    print("")
+
+    print("----- AFFICHAGE DE TOUS LES ÉTUDIANTS-----")
     students = school.get_all_students()
-    courses = school.get_all_courses()
 
     for student in students:
         print(student)
 
-    for teacher in teachers:
-        print(teacher)
+    print("")
+
+    print("----- AFFICHAGE DE TOUS LES COURS-----")
+    courses = school.get_all_courses()
 
     for course in courses:
         print(course)
+    print("")
 
-    student1 = Student("charles", "xavier", 16)
+
+
+
+
+    '''student1 = Student("charles", "xavier", 18)
 
     if school.create_student(student1):
         print("Étudiant créé")
     else:
-        print("Échec de la creation de l'étudiant")
+        print("Échec de la creation de l'étudiant")'''
 
     student_to_modify =  school.get_student_by_id(1)
     student_to_modify.age = 77
